2e1e7db32906f29dcf51b022227e6cc19cadb340
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-26 21:54:03 +0000

    Allow creating a CallBody from a RpcCall impl.

--- src/message/call_body.rs
+++ src/message/call_body.rs
@@ -1,4 +1,5 @@
 use super::auth_data::AuthData;
+use super::super::rpc::RpcCall;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CallBody<T> {
@@ -9,4 +10,21 @@ pub struct CallBody<T> {
     credentials: AuthData,
     verifier: AuthData,
     parameters: T,
+}
+
+impl<T, C> From<C> for CallBody<T>
+where
+    C: RpcCall<Parameters = T>,
+{
+    fn from(rpc_call: C) -> Self {
+        CallBody {
+            rpc_version: 2,
+            program: rpc_call.program(),
+            version: rpc_call.version(),
+            procedure: rpc_call.procedure(),
+            credentials: rpc_call.credentials(),
+            verifier: rpc_call.verifier(),
+            parameters: rpc_call.parameters(),
+        }
+    }
 }
