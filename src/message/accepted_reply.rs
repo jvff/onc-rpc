a235a0e27bdf59265641b0d415fe38665e1b20b5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-26 21:43:07 +0000

    Implement Into<Result<T>> for AcceptedReply<T>

--- src/message/accepted_reply.rs
+++ src/message/accepted_reply.rs
@@ -1,8 +1,15 @@
 use super::accepted_status::AcceptedStatus;
 use super::auth_data::AuthData;
+use super::super::errors::Result;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct AcceptedReply<T> {
     verifier: AuthData,
     reply_data: AcceptedStatus<T>,
+}
+
+impl<T> Into<Result<T>> for AcceptedReply<T> {
+    fn into(self) -> Result<T> {
+        self.reply_data.into()
+    }
 }
