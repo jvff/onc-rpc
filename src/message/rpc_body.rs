304b2723f2db95a152eec281f9c564186ddcc1a9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-26 21:54:36 +0000

    Allow creating a RpcBody from a RpcCall impl.

--- src/message/rpc_body.rs
+++ src/message/rpc_body.rs
@@ -1,11 +1,21 @@
 use super::call_body::CallBody;
 use super::reply_body::ReplyBody;
 use super::super::errors::{ErrorKind, Result};
+use super::super::rpc::RpcCall;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum RpcBody<C, R> {
     Call(CallBody<C>),
     Reply(ReplyBody<R>),
+}
+
+impl<C, R, P> From<P> for RpcBody<C, R>
+where
+    P: RpcCall<Parameters = C, ResultData = R>,
+{
+    fn from(rpc_call: P) -> Self {
+        RpcBody::Call(rpc_call.into())
+    }
 }
 
 impl<C, R> RpcBody<C, R> {
