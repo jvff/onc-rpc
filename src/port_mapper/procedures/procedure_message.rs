09ab2ab97aa82d6837db364582317be6e2aaad7e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-27 12:01:59 +0000

    Add custom deserialize_with_hint() method
    - Determines the ProcedureMessage type based on the RequestId hint

--- src/port_mapper/procedures/procedure_message.rs
+++ src/port_mapper/procedures/procedure_message.rs
@@ -1,9 +1,12 @@
+use serde::{Deserialize, Deserializer};
+
 use super::call_broadcast::CallBroadcast;
 use super::dump::Dump;
 use super::get_port::GetPort;
 use super::null::Null;
 use super::set::Set;
 use super::unset::Unset;
+use super::super::requests::RequestId;
 use super::super::super::message::RpcMessage;
 
 #[derive(Deserialize, Serialize)]
@@ -50,5 +53,39 @@ impl From<RpcMessage<Dump>> for ProcedureMessage {
 impl From<RpcMessage<CallBroadcast>> for ProcedureMessage {
     fn from(message: RpcMessage<CallBroadcast>) -> Self {
         ProcedureMessage::CallBroadcast(message)
+    }
+}
+
+impl ProcedureMessage {
+    pub fn deserialize_with_hint<'de, D>(
+        hint: RequestId,
+        deserializer: D,
+    ) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        match hint {
+            RequestId::Null => {
+                Ok(RpcMessage::<Null>::deserialize(deserializer)?.into())
+            },
+            RequestId::Set => {
+                Ok(RpcMessage::<Set>::deserialize(deserializer)?.into())
+            }
+            RequestId::Unset => {
+                Ok(RpcMessage::<Unset>::deserialize(deserializer)?.into())
+            }
+            RequestId::GetPort => {
+                Ok(RpcMessage::<GetPort>::deserialize(deserializer)?.into())
+            }
+            RequestId::Dump => {
+                Ok(RpcMessage::<Dump>::deserialize(deserializer)?.into())
+            }
+            RequestId::CallBroadcast => {
+                Ok(
+                    RpcMessage::<CallBroadcast>::deserialize(deserializer)?
+                        .into()
+                )
+            }
+        }
     }
 }
