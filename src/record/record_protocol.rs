3fbf940c62ca3b613f2f2636e737df962d1f3ef4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-27 10:24:36 +0000

    Implement RecordProtocol
    - An implementation of tokio_proto::multiplex::ClientProto
    - Use a custom fork of tokio-proto that supports generic error types for
      the protocol

---
+++ src/record/record_protocol.rs
@@ -0,0 +1,23 @@
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_proto::multiplex::ClientProto;
+
+use super::multiplex_record_transport::MultiplexRecordTransport;
+use super::record::Record;
+use super::super::errors::{Error, Result};
+
+pub struct RecordProtocol;
+
+impl<T> ClientProto<T> for RecordProtocol
+where
+    T: AsyncRead + AsyncWrite + 'static,
+{
+    type Request = Record<Vec<u8>>;
+    type Response = Record<Vec<u8>>;
+    type Error = Error;
+    type Transport = MultiplexRecordTransport<T>;
+    type BindTransport = Result<Self::Transport>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(MultiplexRecordTransport::new(io))
+    }
+}
