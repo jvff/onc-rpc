370421dc4ec797b7f7ddd154ebd27515d420fb04
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-27 12:00:42 +0000

    Implement From<RpcMessage<_>> for ProcedureMessage
    - For all valid procedure messages

--- src/port_mapper/procedures/procedure_message.rs
+++ src/port_mapper/procedures/procedure_message.rs
@@ -16,3 +16,39 @@ pub enum ProcedureMessage {
     Dump(RpcMessage<Dump>),
     CallBroadcast(RpcMessage<CallBroadcast>),
 }
+
+impl From<RpcMessage<Null>> for ProcedureMessage {
+    fn from(message: RpcMessage<Null>) -> Self {
+        ProcedureMessage::Null(message)
+    }
+}
+
+impl From<RpcMessage<Set>> for ProcedureMessage {
+    fn from(message: RpcMessage<Set>) -> Self {
+        ProcedureMessage::Set(message)
+    }
+}
+
+impl From<RpcMessage<Unset>> for ProcedureMessage {
+    fn from(message: RpcMessage<Unset>) -> Self {
+        ProcedureMessage::Unset(message)
+    }
+}
+
+impl From<RpcMessage<GetPort>> for ProcedureMessage {
+    fn from(message: RpcMessage<GetPort>) -> Self {
+        ProcedureMessage::GetPort(message)
+    }
+}
+
+impl From<RpcMessage<Dump>> for ProcedureMessage {
+    fn from(message: RpcMessage<Dump>) -> Self {
+        ProcedureMessage::Dump(message)
+    }
+}
+
+impl From<RpcMessage<CallBroadcast>> for ProcedureMessage {
+    fn from(message: RpcMessage<CallBroadcast>) -> Self {
+        ProcedureMessage::CallBroadcast(message)
+    }
+}
