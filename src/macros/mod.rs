cd6b77485ad70c421b3204fac7afa06b7cd87b28
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-28 22:44:01 +0000

    Create onc_rpc_program_procedures! macro
    - Create representations for all listed procedures, as well as a
      ProcedureMessage type

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,5 +1,6 @@
 #[macro_use] mod parameters_macro;
 #[macro_use] mod procedure_macro;
 #[macro_use] mod procedure_message_macro;
+#[macro_use] mod procedures_macro;
 #[macro_use] mod request_macro;
 #[macro_use] mod request_id_macro;
---
+++ src/macros/procedures_macro.rs
@@ -0,0 +1,25 @@
+macro_rules! onc_rpc_program_procedures {
+    (
+        $( $procedure:ident $parameters:tt $( -> $result_type:ty )* ),*
+        $(,)*
+    ) => {
+        mod procedures {
+            use serde::{Deserialize, Deserializer};
+
+            use $crate::{DeserializeWithHint, RpcMessage};
+
+            use super::*;
+            use super::request::{Request, RequestId};
+
+            $(
+                onc_rpc_program_procedure! {
+                    $procedure $parameters $( -> $result_type )*
+                }
+            )*
+
+            onc_rpc_program_procedure_message! {
+                $( $procedure $parameters ),*
+            }
+        }
+    };
+}
