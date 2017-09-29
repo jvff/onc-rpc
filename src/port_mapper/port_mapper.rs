d698cd9a834001340c28bab7b6691f79f30c8469
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-29 16:01:49 +0000

    Use macro exports in port mapper

--- src/port_mapper/port_mapper.rs
+++ src/port_mapper/port_mapper.rs
@@ -6,19 +6,16 @@ onc_rpc! {
     program(port_mapper::PortMapper) {
         id = 100_000;
         version = 2;
+        connect = Connect;
 
         procedures {
             0 => null(),
-            1 => set(program: Mapping) -> bool,
-            2 => unset(program: Mapping) -> bool,
-            3 => get_port(program: Mapping) -> u32,
-            4 => dump() -> Vec<Mapping>,
-            5 => call_broadcast(arguments: CallArgs) -> CallResult,
+            1 => set(program: Mapping) -> bool => SetResult,
+            2 => unset(program: Mapping) -> bool => UnsetResult,
+            3 => get_port(program: Mapping) -> u32 => GetPortResult,
+            4 => dump() -> Vec<Mapping> => DumpResult,
+            5 => call_broadcast(arguments: CallArgs) -> CallResult
+                => CallBroadcastResult,
         }
     }
 }
-
-pub use self::port_mapper::Connect;
-
-pub use self::port_mapper::procedures::get_port::
-    ResponseResult as GetPortResult;
