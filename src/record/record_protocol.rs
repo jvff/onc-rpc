002eacb6f60f29a3a702b8219b35297ad2b26065
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-10-18 17:43:40 +0000

    Implement ServerProto for RecordProtocol
    - The protocol is symmetric for client and server, so the ClientProto
      and ServerProto implementations are identical

--- src/record/record_protocol.rs
+++ src/record/record_protocol.rs
@@ -1,5 +1,5 @@
 use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_proto::multiplex::ClientProto;
+use tokio_proto::multiplex::{ClientProto, ServerProto};
 
 use super::multiplex_record_transport::MultiplexRecordTransport;
 use super::record::Record;
@@ -8,6 +8,21 @@ use super::super::errors::{Error, Result};
 pub struct RecordProtocol;
 
 impl<T> ClientProto<T> for RecordProtocol
+where
+    T: AsyncRead + AsyncWrite + 'static,
+{
+    type Request = Record<Vec<u8>>;
+    type Response = Record<Vec<u8>>;
+    type Error = Error;
+    type Transport = MultiplexRecordTransport<T>;
+    type BindTransport = Result<Self::Transport>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(MultiplexRecordTransport::new(io))
+    }
+}
+
+impl<T> ServerProto<T> for RecordProtocol
 where
     T: AsyncRead + AsyncWrite + 'static,
 {
