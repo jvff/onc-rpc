c328699e34c7573f0e57c948174f06f6b5c7e14b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-11 15:08:41 +0000

    Document RpcProcedure

--- src/rpc/rpc_procedure.rs
+++ src/rpc/rpc_procedure.rs
@@ -1,9 +1,14 @@
 use super::rpc_program::RpcProgram;
 
+/// Specification of a remote procedure interface.
 pub trait RpcProcedure {
+    /// The program that the procedure is associated to.
     type Program: RpcProgram;
+    /// The type that contains all the parameters for the procedure.
     type Parameters: Sized;
+    /// The type of the resulting data.
     type ResultData: Sized;
 
+    /// The procedure identification code.
     fn procedure() -> u32;
 }
