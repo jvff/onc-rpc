1de42cdfba0a7b8a1d75c53ee23189f96df203ad
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-11 19:40:30 +0000

    Document DeserializeWithHint

--- src/service/deserialize_with_hint.rs
+++ src/service/deserialize_with_hint.rs
@@ -1,6 +1,12 @@
 use serde::Deserializer;
 
+/// Additional deserialization method with a hint parameter.
 pub trait DeserializeWithHint<H> {
+    /// Attempts to construct a new type by deserializing using a hint
+    /// parameter.
+    ///
+    /// The hint parameter is normally used as extra information to use during
+    /// the deserialization process.
     fn deserialize_with_hint<'de, D>(
         hint: H,
         deserializer: D,
