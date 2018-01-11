7f42551f62ab0d0a54081c56f017db96952814a3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-11 19:21:48 +0000

    Document RpcProgram

--- src/rpc/rpc_program.rs
+++ src/rpc/rpc_program.rs
@@ -1,4 +1,17 @@
+/// Specification of a remote program.
+///
+/// This conforms to what's specified in the
+/// [RPC Programs and Procedures][rfc_prog] of the RFC.
+///
+/// [rfc_prog]: https://tools.ietf.org/html/rfc1057#page-5
 pub trait RpcProgram {
+    /// Returns the program identification number.
+    ///
+    /// For more information, see the [Program Number Assignment][num_assign]
+    /// section of the RFC.
+    ///
+    /// [num_assign]: https://tools.ietf.org/html/rfc1057#page-7
     fn program() -> u32;
+    /// Returns the program version.
     fn version() -> u32;
 }
