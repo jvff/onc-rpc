43ab3943f18263f9a49745957a76571c8b5c1f3f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-11 19:43:51 +0000

    Document TryFrom

--- src/service/try_from.rs
+++ src/service/try_from.rs
@@ -1,6 +1,11 @@
+/// Attempt to construct a type by conversion.
+///
+/// This is similar to `From`, but may fail.
 pub trait TryFrom<T> {
+    /// The error type of a failed attempt.
     type Error;
 
+    /// Attempt to convert a value into a new instance.
     fn try_from(value: T) -> Result<Self, Self::Error>
     where
         Self: Sized,
