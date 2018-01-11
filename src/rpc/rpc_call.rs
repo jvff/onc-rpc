5c0b5946675584845952b390ff63cc720531c707
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-11 10:52:58 +0000

    Document RpcCall

--- src/rpc/rpc_call.rs
+++ src/rpc/rpc_call.rs
@@ -1,15 +1,23 @@
 use super::rpc_procedure::RpcProcedure;
 use super::super::message::AuthData;
 
+/// Representation of a remote procedure call.
+///
+/// Any type that implements this trait provides enough information to create a
+/// remote procedure call request message.
 pub trait RpcCall {
+    /// The requested procedure.
     type Procedure: RpcProcedure;
 
+    /// The parameters for the call.
     fn parameters(&self) -> <Self::Procedure as RpcProcedure>::Parameters;
 
+    /// The credentials to perform the call.
     fn credentials(&self) -> AuthData {
         AuthData::default()
     }
 
+    /// The verifier to use for the credentials.
     fn verifier(&self) -> AuthData {
         AuthData::default()
     }
