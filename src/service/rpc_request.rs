7d1413b6e26004f90b8ca66c44cea225bc3b5cd0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-18 12:17:09 +0000

    Document RpcRequest

--- src/service/rpc_request.rs
+++ src/service/rpc_request.rs
@@ -1,5 +1,15 @@
+/// Represents an RPC request.
+///
+/// A type that implements this must give a hint on the type of the expected
+/// response of a request message. Normally this trait is applied to an
+/// enumeration of possible requests and has as a response hint an enumeration
+/// mapping each request to a response type.
+///
+/// The hint is used during deserialization of the response message.
 pub trait RpcRequest {
+    /// The type of the hint of the expected response type.
     type ResponseHint: Copy;
 
+    /// Getter method for the response type hint.
     fn response_hint(&self) -> Self::ResponseHint;
 }
