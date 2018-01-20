1260fb8034b50801635a7ec2577ed8271adcb502
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-20 01:31:17 +0000

    Document CallArgs

--- src/port_mapper/call_args.rs
+++ src/port_mapper/call_args.rs
@@ -1,5 +1,10 @@
 use serde_bytes;
 
+/// Arguments for forwarded call request.
+///
+/// When a port mapper is called to forward a remote call request, these
+/// parameters are necessary to select the program instance, the procedure to
+/// call and what arguments should be used.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CallArgs {
     program: u32,
