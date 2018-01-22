a0a4c5a0c5ab6b4057ea439947b132bb32247e94
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-22 18:00:26 +0000

    Document CallResult

--- src/port_mapper/call_result.rs
+++ src/port_mapper/call_result.rs
@@ -1,5 +1,9 @@
 use serde_bytes;
 
+/// Result of a forwarded call request.
+///
+/// Data returned by the remote procedure of another program called through the
+/// port mapper program.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CallResult {
     port: u32,
