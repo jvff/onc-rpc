88740bbfbaaf80db1da6eb510613ab29edc90389
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-22 18:09:58 +0000

    Document Mapping

--- src/port_mapper/mapping.rs
+++ src/port_mapper/mapping.rs
@@ -1,14 +1,23 @@
 use super::protocol::Protocol;
 
+/// A mapping between a program instance and a port number.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Mapping {
+    /// Program ID number.
     pub program: u32,
+
+    /// Program version.
     pub version: u32,
+
+    /// Protocol used to communicate.
     pub protocol: Protocol,
+
+    /// Port used to communicate.
     pub port: u32,
 }
 
 impl Mapping {
+    /// Create a mapping representing a program instance.
     pub fn of_program(program: u32, version: u32) -> Self {
         Mapping {
             program,
